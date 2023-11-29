04858dc6014af9857be817b94fa44d2041e32d64
mAftabAslam <maftabaslam1249@gmail.com>
2023-11-29 22:17:36 +0500

    Fix issue #3 Devolope Log In and Sign Up

--- TrainEase/TrainEase/MainWindow.xaml.cs
+++ TrainEase/TrainEase/MainWindow.xaml.cs
@@ -1,30 +1,65 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using TrainEase.ViewModels;
+using TrainEase.Data;
 
 namespace TrainEase
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-            DataContext = new MainWindowViewModel();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Password;
+
+            if (IsValidUser(username, password))
+            {
+                // Navigate to the main application window or perform other actions
+                MessageBox.Show("Login successful!");
+            }
+            else
+            {
+                lblLoginError.Visibility = Visibility.Visible;
+                lblLoginError.Text = "Invalid username or password.";
+            }
+        }
+        private void btnSignUp_Click(object sender, RoutedEventArgs e)
+        {
+            SignUp signUpControl = new SignUp();
+            signUpControl.eve += SignUpControl_SignUpCompleted;
+
+            // Create a pop-up window and set its content to the SignUp user control
+            Window signUpWindow = new Window
+            {
+                Title = "Sign Up",
+                Content = signUpControl,
+                SizeToContent = SizeToContent.WidthAndHeight,
+                ResizeMode = ResizeMode.NoResize
+            };
+
+            // Show the pop-up window
+            signUpWindow.ShowDialog();
+        }
+
+        private void SignUpControl_SignUpCompleted(object sender, System.EventArgs e)
+        {
+            // Handle sign-up completion, e.g., close the sign-up window or navigate to the main application window
+            MessageBox.Show("Sign-up successful!");
+        }
+        private bool IsValidUser(string username, string password)
+        {
+            // Add logic to check if the user exists in the database
+            // For demonstration purposes, let's assume there is a Users table
+            // in your TrainEaseDBContext with columns Username and Password
+            using (var context = new TrainEaseDbContext())
+            {
+                return context.Users.Any(u => u.Username == username && u.Password == password);
+            }
         }
     }
 }
